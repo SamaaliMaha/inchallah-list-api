db4de89f8c4bde197a1f149f1803009d4f05b341
maha smaali <mahasmaali18@gmail.com>
2021-04-18 15:05:24 +0200

    parcelleapi

---
+++ controllers/parcelleController.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+
+const Task = require('./../models/parcelle')
+
+const app = express()
+
+app.post('/', async (req, res) => {
+    try {
+        let data = req.body
+        //get clé du token authorization
+        let token = req.get('Authorization').substring(7)
+        //decrypt token and take only userid from it
+        let ownerid = jwt.verify(token, "SECRETKEY").userid
+
+        let task = new Parcelle({
+            description: data.description,
+            adresse:data.adresse,
+            NaturedeProduction: data.NaturedeProduction,
+            RendementAttendu: data.RendementAttendu,
+            PrixdeVente: data.PrixdeVente,
+            RisquesPossibles: data.RisquesPossibles,
+            NaturedeStock: data.NaturedeStock,
+            Qtte: data.Qtte,
+            Prix: data.Prix,
+            ValeurduBatiment: data.ValeurduBatiment,
+            ValeurdeProduit: data.ValeurdeProduit,
+            Commentaire: data.Commentaire,
+            owner: ownerid
+        })
+
+        await parcelle.save()
+
+        res.status(201).send({ msg: "parcelle added" })
+    }
+    catch (error) {
+        res.status(400).send({ msg: "error" })
+    }
+})
+app.get('/', async (req, res) => {
+    try {
+        let token = req.get('Authorization').substring(7)
+        let ownerid = jwt.verify(token, "SECRETKEY").userid
+
+        let parcelles = await Parcelle.find({ owner: ownerid })
+
+        res.status(200).send(parcelles)
+    }
+    catch (error) {
+        res.status(400).send({ msg: "error" })
+    }
+})
+module.exports = app
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const nodemailer = require("nodemailer");
 
 const User = require('./../models/user')
 
@@ -60,6 +61,62 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/forgot-password', async (req, res) => {
+    try {
+        //recuperation data
+        let data = req.body
+        //verify email
+        let user = await User.findOne({ email: data.email })
+
+        if (!user) {
+            res.status(404).send({ msg: "user not found" })
+        } else {
+
+            let newPassword = generateNewPassword(10)
+
+            user.password = bcrypt.hashSync(newPassword, bcrypt.genSaltSync(10))
+
+            await user.save()
+
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: "mahasmaali18@gmail.com", // generated ethereal user
+                    pass: mdp.pass, // generated ethereal password
+                },
+            });
+
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: '"maha smaali " <mahasmaali18@gmail.com>', // sender address
+                to: user.email, // list of receivers
+                subject: "Nouveau mot de passe ✔", // Subject line
+                text: "Nouveau mot de passe : " + newPassword, // plain text body
+                html: "<b>Nouveau mot de passe : " + newPassword + "</b>", // html body
+            });
+
+
+            res.status(200).send({ message: "Password updated" })
+        }
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+function generateNewPassword(length) {
+    var result = [];
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result.push(characters.charAt(Math.floor(Math.random() *
+            charactersLength)));
+    }
+    return result.join('');
+}
 
 //export app
 module.exports = app
